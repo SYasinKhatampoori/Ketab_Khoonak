e7bc95cb98a7167205c6fc760f710b7e72fefb6e
SYasinKhatampoori <khatmpwryy@gmail.com>
2025-12-09 02:04:08 -0800

    add items

---
+++ khoonak/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-evenly flex-col items-center h-screen">
+    <header class="absolute top-0 flex justify-evenly items-center w-full h-18 gap-x-10 bg-[#A67B5B] rounded-b-lg">
+      <div class="cursor-pointer border border-black rounded-full p-3 bg-[#f7c390]">
+        <img src="./assets/menu-svgrepo-com.svg" class="w-6" alt="">
+      </div>
+      <div>
+
+      </div>
+      <div class="bg-[#f7c390] px-2 py-3 rounded-lg shadow-[0_4px_10px_#f7c390] cursor-pointer duration-75 hover:px-3 hover:py-3.5">
+        <router-link to="/login" class=""><span>ورود و ثبت نام</span></router-link>
+      </div>
+      
+    </header>
+
+    
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ khoonak/src/views/LoginView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex justify-eve items-center flex-col">
+    <form
+      action=""
+      class="w-[350px] h-[400px] bg-[#A67B5B] p-10 rounded-lg shadow-2xl flex justify-between items-center flex-col"
+    >
+      <h1 class="text-2xl text-[#FFF8EA]">ورود</h1>
+      <span class="w-full h-1 bg-[#FFF8EA] rounded-3xl"></span>
+      <div
+        class="flex justify-between items bg-center text-xl text-[#A67B5B] rounded-lg h-10"
+      >
+        <label
+          class="text-[#A67B5B] font-bold p-[1.5px] bg-[#f7c390] w-[60px] rounded-r-lg border-black text-sm flex justify-center items-center"
+          >نام کاربری</label
+        >
+        <input
+          type="text"
+          class="rounded-l-lg outline-none p-2 text-[#A67B5B] bg-[#FFF8EA]"
+        />
+      </div>
+      <div
+        class="flex justify-between items bg-center text-xl text-[#A67B5B] rounded-lg h-10"
+      >
+        <label
+          class="text-[#A67B5B] font-bold p-[1.5px] bg-[#f7c390] w-[60px] rounded-r-lg border-black text-sm flex justify-center items-center"
+          >رمز</label
+        >
+        <input
+          type="password"
+          class="rounded-l-lg outline-none p-2 text-[#A67B5B] bg-[#FFF8EA]"
+        />
+      </div>
+      
+      <button
+        type="submit"
+        class="text-xl text-[#FFF8EA] bg-[#f7c390] flex items-center justify-center w-full px-5 py-3 rounded-lg"
+      >
+        ورود
+      </button>
+
+      <p class="text-[#FFF8EA] bg-[#A67B5B] p-2 border-black text-md">
+        حساب ندارید
+        <a href="" class="text-[#FFF8EA] hover:text-[#d3c3a3]">پس بسازید</a>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
